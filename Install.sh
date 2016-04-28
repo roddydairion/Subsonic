5abb94b7e69e6fb24e261bd9f176b80f89db6e71
Roddy Dairion <roddy.dairion@appletree.co.uk>
2016-04-28 15:36:38 +0100

    Installation script

--- Install.sh
+++ Install.sh
@@ -3,11 +3,13 @@ export PATH=$PATH:/usr/local/bin
 sudo yum install -y java-1.7.0-openjdk
 subsonicURL="http://subsonic.org/download/"
 subsonicFILE="subsonic-6.0.beta2.rpm"
-echo -n "Enter subsonic file name to download (default ${subsonicFILE}): "
+wget -O subsonic.rpm https://sourceforge.net/projects/subsonic/files/latest/download?source=files
+
+echo -n "Enter subsonic file name to download (Leave blank to download latest version): "
 read text
 if [ -z "$text"]
 then
-	subFile="$subsonicFILE"
+	subFile=wget -O subsonic.rpm https://sourceforge.net/projects/subsonic/files/latest/download?source=files
 else
 	subFile="$text"
 fi
