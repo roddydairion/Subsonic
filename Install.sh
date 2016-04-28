3c4baef6bd8fcd73fa13fc42e05272e93d22500d
Roddy Dairion <roddy.dairion@appletree.co.uk>
2016-04-28 14:23:15 +0100

    Installation script

---
+++ Install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#sudo yum install java-1.7.0-openjdk
+subsonicURL="http://subsonic.org/download/"
+subsonicFILE="subsonic-6.0.beta2.rpm"
+echo -n "Enter subsonic file name to download (default ${subsonicFILE}): "
+read text
+if [ -z "$text"]
+then
+	subFile="$subsonicFILE"
+else
+	subFile="$text"
+fi
+echo "${subsonicURL}${subFile}"
+#wget "${subsonicURL}${subFile}"
+#sudo yum install --nogpgcheck "${subsonicFILE}"
+#sudo yum remove subsonic
+#rm -rf "${subsonicURL}${subsonicFILE}"
