3fc60a3445d93d752b1b14a62b1aef52d4fae5cf
Roddy Dairion <roddy.dairion@appletree.co.uk>
2016-04-29 13:11:21 +0100

    Install

--- Install.sh
+++ Install.sh
@@ -1,23 +1,22 @@
 #!/bin/sh
 export PATH=$PATH:/usr/local/bin
+sudo yum remove -y subsonic
 sudo yum install -y java-1.7.0-openjdk
 subsonicURL="http://subsonic.org/download/"
 subsonicFILE="subsonic-6.0.beta2.rpm"
-wget -O subsonic.rpm https://sourceforge.net/projects/subsonic/files/latest/download?source=files
 
 echo -n "Enter subsonic file name to download (Leave blank to download latest version): "
 read text
 if [ -z "$text"]
 then
-	subFile=wget -O subsonic.rpm https://sourceforge.net/projects/subsonic/files/latest/download?source=files
+	wget -O subsonic.rpm https://sourceforge.net/projects/subsonic/files/latest/download?source=files
+	subsonicFILE="subsonic.rpm"
 else
-	subFile="$text"
+	wget "${subsonicURL}${subFile}"
 fi
-#echo "${subsonicURL}${subFile}"
-echo "Removing old Subsonic installation"
-sudo yum remove -y subsonic
-wget "${subsonicURL}${subFile}"
-sudo yum install -y --nogpgcheck "${subsonicFILE}"
 
+echo "Removing old Subsonic installation"
+
+sudo yum install -y --nogpgcheck "${subsonicFILE}"
 rm -rf "${subsonicFILE}"
 sudo iptables -I INPUT -p tcp --dport 4040 -j ACCEPT
