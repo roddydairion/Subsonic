6c7fb569352fb699fd96aca4ba07f1d5a156b38d
Roddy Dairion <roddy@orange.mu>
2016-04-30 23:27:49 +0400

    Install update

--- Install.sh
+++ Install.sh
@@ -65,7 +65,8 @@ main()
 	detect_os
 	filename=$(curl -sIkL https://sourceforge.net/projects/subsonic/files/latest/download?source=files | sed -r '/filename=/!d;s/.*filename=(.*)$/\1/')
 	formatfile=$(echo "${filename}" | sed 's/\"//g')
-	@arr=split(/./, $formatfile)
+	
+	@arr=split(/./, $formatfile);
 
 	
 	if [ $os == "centos" ]
