429fb550e4484919a8295d44fe3c510948d30176
root <root@roddy.wikaba.com>
2016-04-30 18:24:13 -0400

    Configuration file

--- RunConfig.sh
+++ RunConfig.sh
@@ -8,28 +8,30 @@ gpasswd --add syssubsonic audio
 
 if [ $os == "centos" ]
 then
-	fileLoc="/etc/sysconfig/subsonic"	
+        fileLoc="/etc/sysconfig/subsonic"
 elif [ $os == "ubuntu" ]
 then
-	fileLoc="/etc/default/subsonic"
+        fileLoc="/etc/default/subsonic"
 else
-	echo "No possible OS found to create configuration!"
-	exit
+        echo "No possible OS found to create configuration!"
+        exit
 fi
 
 if /bin/grep -q "SUBSONIC_USER=root" "${fileLoc}"
 then
-	$(/bin/sed -i -e 's/\SUBSONIC_USER=root/SUBSONIC_USER=syssubsonic/g' $fileLoc)
+        $(/bin/sed -i -e 's/\SUBSONIC_USER=root/SUBSONIC_USER=syssubsonic/g' $fileLoc)
 else
-	echo "Cannot change user. Open /etc/sysconfig/subsonic to manually change SUBSONIC_USER"
+        echo "Cannot change user. Open /etc/sysconfig/subsonic to manually change SUBSONIC_USER"
 fi
 
 if /bin/grep -q "SUBSONIC_ARGS=\"--max-memory=150\"" "${fileLoc}"
 then
-	$(/bin/sed -i -e 's/\SUBSONIC_ARGS="--max-memory=150"/SUBSONIC_ARGS="--https-port=8404 --max-memory=150"/g' $fileLoc)
+        $(/bin/sed -i -e 's/\SUBSONIC_ARGS="--max-memory=150"/SUBSONIC_ARGS="--https-port=8404 --max-memory=150"/g' $fileLoc)
 else
-	echo "Cannot change port number.  Open /etc/sysconfig/subsonic to manually change SUBSONIC_ARGS=\"--max-memory=150\" to SUBSONIC_ARGS=\"--https-port=8404 --max-memory=150\""
+        echo "Cannot change port number.  Open /etc/sysconfig/subsonic to manually change SUBSONIC_ARGS=\"--max-memory=150\" to SUBSONIC_ARGS=\"--https-port=8404 --max-memory=150\""
 fi
 
 sudo iptables -I INPUT -p tcp --dport 8404 -j ACCEPT
 service subsonic start
+
+
